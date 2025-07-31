04569c58ffa48b6fa7fef5fc36fa8bf5dc9c8895
Alexey <lp@tdsgn.ru>
2025-07-31 22:43:52 +0700

    #000 feat: add check large number of lines, check UTF-8 charset

--- src/main/java/org/SortVariables/InputFile.java
+++ src/main/java/org/SortVariables/InputFile.java
@@ -2,9 +2,18 @@ package org.SortVariables;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.math.BigInteger;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CodingErrorAction;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Класс для проверки и интерпретации входного файла
@@ -39,56 +48,101 @@ public class InputFile {
         String currentDirectory = System.getProperty("user.dir");
         String filePath = currentDirectory + this.INPUT_FILES_FOLDER + fileName;
 
+        this.checkCharsetFile(filePath);
+
+        this.setFileValuesWithType(filePath);
+    }
+
+    /**
+     * Создать список с переменными и их типами
+     *
+     * @param filePath Путь к файлу
+     */
+    private void setFileValuesWithType(String filePath) {
         try (Scanner scanner = new Scanner(new File(filePath))) {
-            this.setFileValuesWithType(scanner);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+
+                if (line.isEmpty()) {
+                    continue;
+                }
+
+                String type = this.checkValueType(line);
+
+                this.addVariableWithType(type, line);
+            }
         } catch (FileNotFoundException error) {
             this.setIsHasErrors(true);
-            this.addMessageErrorMessageList("Не удалось найти файл - " + fileName);
-            this.addMessageErrorMessageList("Полный путь до файла: " + filePath);
+            this.addMessageErrorMessageList("Не удалось найти файл: " + filePath);
             this.addMessageErrorMessageList("");
         }
     }
 
-    private void setFileValuesWithType(Scanner scanner) {
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-
-            if (line.isEmpty()) {
-                continue;
-            }
-
-            boolean isInteger;
-            boolean isFloat;
-            String type;
-
+    /**
+     * Проверить файл на UTF-8 кодировку
+     *
+     * @param filePath
+     */
+    private void checkCharsetFile(String filePath) {
+        Path path = Paths.get(filePath);
+        try{
+            ByteBuffer buffer = ByteBuffer.wrap(Files.readAllBytes(path));
+            Charset charset = StandardCharsets.UTF_8;
+            CharsetDecoder decoder = charset.newDecoder();
+            decoder.onMalformedInput(CodingErrorAction.REPORT);
+            decoder.onUnmappableCharacter(CodingErrorAction.REPORT);
             try {
-                new BigInteger(line);
-                isInteger = true;
-            } catch (NumberFormatException error) {
-                isInteger = false;
+                decoder.decode(buffer.duplicate());
+            } catch (Exception e) {
+                this.setIsHasErrors(true);
+                this.addMessageErrorMessageList("Проблема с кодировкой файла: " + filePath);
+                this.addMessageErrorMessageList("");
             }
+        } catch (IOException error) {
+            System.err.println("Ошибка при работе с файлом: " + error.getMessage());
+            System.out.println();
+            System.exit(1);
+        }
+    }
 
-            if (!isInteger) {
-                try {
-                    Double.parseDouble(line);
-                    isFloat = true;
-                } catch (NumberFormatException error) {
-                    isFloat = false;
-                }
-            } else {
+    /**
+     * Узнать тип переменной
+     *
+     * @param value Переменная
+     * @return Тип
+     */
+    private String checkValueType(String value){
+        boolean isInteger;
+        boolean isFloat;
+        String type;
+
+        try {
+            new BigInteger(value);
+            isInteger = true;
+        } catch (NumberFormatException error) {
+            isInteger = false;
+        }
+
+        if (!isInteger) {
+            try {
+                Double.parseDouble(value);
+                isFloat = true;
+            } catch (NumberFormatException error) {
                 isFloat = false;
             }
-
-            if (isInteger) {
-                type = "int";
-            } else if (isFloat) {
-                type = "float";
-            } else {
-                type = "string";
-            }
-
-            this.addVariableWithType(type, line);
+        } else {
+            isFloat = false;
         }
+
+        if (isInteger) {
+            type = "int";
+        } else if (isFloat) {
+            type = "float";
+        } else {
+            type = "string";
+        }
+
+        return type;
     }
 
     public String[] getErrorMessages() {
@@ -121,6 +175,12 @@ public class InputFile {
      * @param variable Значение стоки из файла
      */
     public void addVariableWithType(String variableType, String variable) {
+        if (variablesWithTypeList.length == Integer.MAX_VALUE) {
+            System.err.println("Слишком большое количество строк во входном файле");
+            System.out.println();
+            System.exit(1);
+        }
+
         String[] newVariableWithTypeList = Arrays.copyOf(this.variablesWithTypeList, this.variablesWithTypeList.length + 2);
         newVariableWithTypeList[newVariableWithTypeList.length - 2] = variableType;
         newVariableWithTypeList[newVariableWithTypeList.length - 1] = variable;
